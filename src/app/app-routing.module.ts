1205be5cf5d37b604efbfbf2f2a56e24f9be5b70
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-12-21 19:41:00 +0100

    routingandClasses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddrdvComponent } from './addrdv/addrdv.component';
+import { ReadrdvComponent } from './readrdv/readrdv.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'accueil',pathMatch:'full'},
+  {path:'accueil',component:AddrdvComponent},
+  {path:'readrdv',component:ReadrdvComponent},
+  {path:'details/:id',component:DetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/rdv.ts
@@ -0,0 +1,7 @@
+export class Rdv {
+    titre!: string;
+    description!: string;
+    date!: string;
+    lieu!:string;
+    montant?:number=0;
+}
